b5442bfd806a3f1ad4e5331986f02b304401bdc4
Matthew Brett <matthew.brett@gmail.com>
2016-02-19 17:11:45 -0800

    Adapt model, family check to standard calculation

--- test_cpuid.py
+++ test_cpuid.py
@@ -11,9 +11,13 @@ CPU_INFO = get_cpu_info()
 
 def test_against_cpuinfo():
     assert info.vendor == CPU_INFO['vendor_id']
-    for attr_name in ('model', 'family', 'extended_family', 'extended_model',
-                      'stepping', 'processor_type'):
+    for attr_name in ('extended_family', 'extended_model', 'stepping',
+                      'processor_type'):
         assert getattr(info, attr_name) == CPU_INFO[attr_name]
+    # See:
+    # http://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-vol-2a-manual.html
+    assert CPU_INFO['model'] == info.model + (info.extended_model << 4)
+    assert CPU_INFO['family'] == info.family + (info.extended_family << 4)
 
 
 def test_smoke():
