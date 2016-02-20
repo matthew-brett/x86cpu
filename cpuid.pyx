929d29f3d8c4167399adc20b71135515355b8603
Matthew Brett <matthew.brett@gmail.com>
2016-02-19 17:00:14 -0800

    Refactor information into Cython extension type

--- cpuid.pyx
+++ cpuid.pyx
@@ -1,6 +1,6 @@
 # -*- Mode: Python -*-
 """
-Get cpuid
+Read CPU information using CPUID instruction.
 """
 
 from collections import namedtuple
@@ -26,51 +26,49 @@ cdef extern from "_cpuid.h":
     int os_supports_avx(e_registers_t cpuid_1)
 
 
-cpdef e_registers_t get_cpuid(uint32_t op):
-    """ Return contents of extended registers after _cpuid call with EAX=`op`
-    """
+cdef class X86Info:
     cdef:
-        e_registers_t reg
-    read_cpuid(op, &reg)
-    return reg
+        readonly e_registers_t reg0, reg1
+        readonly char vendor[32]
+        readonly int stepping, model, family, processor_type
+        readonly int extended_model, extended_family
+
+    def __cinit__(self):
+        cdef:
+            cpu_classifiers_t cpu_classifiers
+        read_cpuid(0, &self.reg0)
+        read_cpuid(1, &self.reg1)
+        read_vendor_string(self.reg0, self.vendor)
+        read_classifiers(self.reg1, &cpu_classifiers)
+        self.stepping = cpu_classifiers.stepping
+        self.model = cpu_classifiers.model
+        self.family = cpu_classifiers.family
+        self.processor_type = cpu_classifiers.processor_type
+        self.extended_model = cpu_classifiers.extended_model
+        self.extended_family = cpu_classifiers.extended_family
+
+    property supports_avx:
+        def __get__(self):
+            """ Return True if CPU and OS support AVX instructions
+
+            The cpuid(1) ECX register tells us if the CPU supports AVX.
+
+            For the OS to support AVX, it needs to preserve the AVX YMM registers when
+            doing a context switch.  In order to do this, the needs to support the
+            relevant instructions, and the OS needs to know to preserve these
+            registers.
+
+            See:
+            * https://en.wikipedia.org/wiki/CPUID
+            * https://en.wikipedia.org/wiki/Advanced_Vector_Extensions
+            * https://software.intel.com/en-us/blogs/2011/04/14/is-avx-enabled/
+
+            Returns
+            -------
+            avx_ok : bool
+                True if CPU and OS support AVX
+            """
+            return bool(os_supports_avx(self.reg1))
 
 
-def get_vendor():
-    """ Return vendor string by querying cpuid
-    """
-    cdef:
-        char vendor[12]
-    read_vendor_string(get_cpuid(0), vendor)
-    return vendor
-
-
-def supports_avx():
-    """ Return True if CPU and OS support AVX instructions
-
-    The cpuid(1) ECX register tells us if the CPU supports AVX.
-
-    For the OS to support AVX, it needs to preserve the AVX YMM registers when
-    doing a context switch.  In order to do this, the needs to support the
-    relevant instructions, and the OS needs to know to preserve these
-    registers.
-
-    See:
-    * https://en.wikipedia.org/wiki/CPUID
-    * https://en.wikipedia.org/wiki/Advanced_Vector_Extensions
-    * https://software.intel.com/en-us/blogs/2011/04/14/is-avx-enabled/
-
-    Returns
-    -------
-    avx_ok : bool
-        True if CPU and OS support AVX
-    """
-    return bool(os_supports_avx(get_cpuid(1)))
-
-
-def get_classifiers():
-    """ Return CPU family, model, extended model / family, stepping, type
-    """
-    cdef:
-        cpu_classifiers_t cpu_classifiers
-    read_classifiers(get_cpuid(1), &cpu_classifiers)
-    return cpu_classifiers
+info = X86Info()
--- test_cpuid.py
+++ test_cpuid.py
@@ -1,20 +1,22 @@
 """ Testing cpuid module
 """
 
-from cpuid import get_vendor, get_classifiers, supports_avx, get_cpuid
+from cpuid import info
 
 from cpuinfo.cpuinfo import get_cpu_info
 
 CPU_INFO = get_cpu_info()
 
-def test_smoke():
-    avx = supports_avx()
-    cpuid = get_cpuid(0)
 
 
 def test_against_cpuinfo():
-    assert get_vendor() == CPU_INFO['vendor_id']
-    classifiers = get_classifiers()
+    assert info.vendor == CPU_INFO['vendor_id']
     for attr_name in ('model', 'family', 'extended_family', 'extended_model',
                       'stepping', 'processor_type'):
-        assert classifiers[attr_name] == CPU_INFO[attr_name]
+        assert getattr(info, attr_name) == CPU_INFO[attr_name]
+
+
+def test_smoke():
+    avx = info.supports_avx
+    cpuid_0 = info.reg0
+    cpuid_1 = info.reg1
